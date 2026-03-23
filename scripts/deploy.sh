40d0cf7c4e17b4a3ecae6afcfd43045f5e307ce5
Nguyen_thanh_thien <thien105715@donga.edu.vn>
2026-03-22 20:29:19 -0700

    hoanthanh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
-o "Deploying server..."
+#!/bin/bash
+echo "Deploying server..."
 
 sudo yum install nginx -y
-sudo cp ../nginx/nginx.conf /etc/nginx/conf.d/default.conf
+sudo cp ./nginx/nginx.conf /etc/nginx/conf.d/default.conf
 sudo systemctl restart nginx
 
 echo "Deploy done!"
