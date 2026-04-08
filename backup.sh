52c7de82c165f342948500d476b3c7fce5ba781b
Thientao0609 <nguyenthanhthien0609@gmail.com>
2026-04-08 00:32:42 -0700

    update code

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 /bin/bash
 cp /etc/ssh/sshd_config /root/backup_sshd_config
-ho "Backu completed nguyen thanh thien 105715 ST23A mai van thuan"
+h "Backu completed nguyen thanh thien 105715 ST23A mai van thuan and truongnam son"
